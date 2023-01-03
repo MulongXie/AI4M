73ef9535638fece25b1e48ca04c87d49c70f1ea7
MulongXie <dsh15325@163.com>
2023-01-03 12:58:37 +1100

    add right sidebar and conversation cards;

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 $(document).ready(()=>{
 
-    let userSelect = $('.user-icon-select')
-    let userNonSelect = $('.user-icon-nonselect')
     // Slide user type while hovering
     $('.user-icon-container').hover(() =>{
-        userNonSelect.css({
+        $('.user-icon-nonselect').css({
             'transform': 'translateX(40px)'
         })
-        userSelect.css({
+        $('.user-icon-select').css({
             'box-shadow': 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'
         })
     }, ()=>{
-        userNonSelect.css({
+        $('.user-icon-nonselect').css({
             'transform': 'translateX(-35px)'
         })
-        userSelect.css({
+        $('.user-icon-select').css({
             'box-shadow': 'rgba(0, 0, 0, 0.35) 0px 5px 15px'
         })
     })
 
     // Switch user
-
+    $('.user-icon-nonselect').click(() => {
+    })
 
 })
