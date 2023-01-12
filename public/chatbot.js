f9a09836681f69265b46b4e34cb219dd4816c622
MulongXie <dsh15325@163.com>
2023-01-12 16:22:55 +1100

    prepare questions and options

---
+++ public/chatbot.js
@@ -0,0 +1,11 @@
+$(document).ready(()=> {
+    const questions = ["What is your role on the AI4M project?",
+    "Which industry sector is your AI4M project targeted at?",
+    "Which business areas does your AI4M project target?",
+    "What types of AI system are you developing?"]
+    
+    const options = [["Lead", "Technician", "Consultant", "Client"],
+    ["Health", "Mining", "Mining", "Law", "Finance", "Agribusiness", "Cyber Security", "Education", "Defence", "Infrastructure", "Manufacturing", "R&D or Innovation", "Environment"],
+    ["Accounting and finance", "Customer service", "Human resources", "IT", "Legal, risk and compliance", "Supply chain", "Marketing", "Research and development", "Sales", "Strategy", "Other"],
+    ["Recognition systems", "Language processing", "Automated decision making", "Recommender systems", "Computer vision", "Other"]]
+})
