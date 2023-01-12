637c63b5b059b99b5c8be5d52b6d83ba883cd854
MulongXie <dsh15325@163.com>
2023-01-12 17:31:25 +1100

    move chatbot into index.js

--- public/index.js
+++ public/index.js
@@ -312,4 +312,20 @@ $(document).ready(()=>{
             '    <p class="con-card-content">' + content + '</p>\n' +
             '</div>'
     }
+
+
+    // ***********
+    // Chatbot
+    // ***********
+    const questions = ["What is your role on the AI4M project?",
+        "Which industry sector is your AI4M project targeted at?",
+        "Which business areas does your AI4M project target?",
+        "What types of AI system are you developing?"]
+
+    const options = [["Lead", "Technician", "Consultant", "Client"],
+        ["Health", "Mining", "Mining", "Law", "Finance", "Agribusiness", "Cyber Security", "Education", "Defence", "Infrastructure", "Manufacturing", "R&D or Innovation", "Environment"],
+        ["Accounting and finance", "Customer service", "Human resources", "IT", "Legal, risk and compliance", "Supply chain", "Marketing", "Research and development", "Sales", "Strategy", "Other"],
+        ["Recognition systems", "Language processing", "Automated decision making", "Recommender systems", "Computer vision", "Other"]]
+
+
 })
